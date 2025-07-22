23aa84f5fa7fcb7de87159d084734fae4e03d135
NuraliyevKenann <kenann.nuraliyev@gmail.com>
2025-07-22 19:54:04 +0400

    bildiyimizpoxi

---
+++ again_example_file.py
@@ -0,0 +1,6 @@
+ali=5
+kenan=15
+if ali>kenan:
+    print("Kenan is lox")
+else:
+    print("Ali is makaka i pox")
---
+++ example_file.py
@@ -0,0 +1,7 @@
+kenan = "POX"
+ali = "bildiyimiz pox"
+
+if kenan == ali:
+    print("Say poxi poxi")
+else:
+    print("Pox nedi qaqa")
